1da585f7b919102fbbfe128a69b832fe2c6aceb7
MicaelaGrande <micaela.grande@mi.unc.edu.ar>
2026-03-06 16:45:09 -0300

    feat: add application enums

---
+++ app/enums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Day_Name(str, Enum):
+    LUNES = "lunes"
+    MARTES = "martes"
+    MIERCOLES = "miercoles"
+    JUEVES = "jueves"
+    VIERNES = "viernes"
+    SABADO = "sabado"
+    DOMINGO = "domingo"
